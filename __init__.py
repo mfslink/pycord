20ad23e487042d1909617d46c47cdda2fae6e1f1
mfslink <dal2741.twitch@gmail.com>
2021-05-18 21:52:44 -0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+__title__ = 'Pycord'
+__author__ = 'Pycord Developers'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present Pycord Developers'
+__version__ = '0.1.0'
+
+
+from .main import *
